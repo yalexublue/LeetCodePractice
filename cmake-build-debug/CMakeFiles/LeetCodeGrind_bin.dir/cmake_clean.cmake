e88e236bd2432d5a7f834a9003e9e1230210882b
yalexublue <yalexu.blue@gmail.com>
2021-10-06 19:31:03 -0400

    a

---
+++ cmake-build-debug/CMakeFiles/LeetCodeGrind_bin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LeetCodeGrind_bin.dir/src/main.cpp.o"
+  "LeetCodeGrind_bin"
+  "LeetCodeGrind_bin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LeetCodeGrind_bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/LeetCodeGrind_bin.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LeetCodeGrind_bin.
+# This may be replaced when dependencies are built.
