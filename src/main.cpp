3369b9c9ef044ac72fec9c67618dc4aba122f937
yalexublue <yalexu.blue@gmail.com>
2021-10-07 05:22:59 -0400

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 #include "ValidPalindrome.h"
+
 using namespace std;
 
 int main(){
-    //validPalindrome check
+    //validPalindrome check II
     string s = "BBCCCB";
     cout << "test for valid palindrome\n"; 
     cout << "YX result: " << validPalindrome_YX(s) << endl;
     cout << "YW reuslt: " << validPalindrome_YW(s) << endl;
+
+    //706. Design HashMap
+
 }
