65a63e16ede2a0a8cdbe3d0f169ed5f896da5a96
asuka1h <77903223+asuka1h@users.noreply.github.com>
2021-10-11 20:44:31 -0400

    1hHashmap

--- src/MyHashMap.h
+++ src/MyHashMap.h
@@ -77,7 +77,75 @@ public:
 };
 
 class MyHashMap_YW {
-    //TODO: Yihan's code here.
+    class MyHashMap {
+    
+private:
+    vector<list<pair<int, int>>> arr;
+    int hashfunction(int key){
+        int ret = 3*key + 10;
+        ret = ret % 10007;
+        ret = ret %57;
+        return ret;
+    }
+public:
+    /** Initialize your data structure here. */
+    MyHashMap(): arr(57) {}
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        
+        int inx = hashfunction(key);
+        list<pair<int, int>>& currList = arr[inx];
+        bool isFound = false;
+        for(auto i = currList.begin(); i != currList.end(); ++i){
+            if(i->first == key){
+                i->second = value;
+                isFound = true;
+            }
+        }
+      
+        if(!isFound){
+            currList.emplace_back(key,value);
+                  
+        }
+       
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        int inx = hashfunction(key);
+        list<pair<int, int>>&  currList= arr[inx];
+         for(auto i = currList.begin(); i != currList.end(); ++i){
+            if(i->first == key){
+                return i->second;
+            }
+            
+        }
+        return -1;
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        int inx = hashfunction(key);
+        list<pair<int, int>>&  currList = arr[inx];
+         for(auto i = currList.begin(); i != currList.end(); ++i){
+            if(i->first == key){
+                currList.erase(i);
+                break;
+            }
+            
+        }
+      
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
 };
 
 #endif
